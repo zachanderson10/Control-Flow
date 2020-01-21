f476959fec361a64850372f209d31b9f2fadc8ad
zach <02zachary.anderson@gulllakecs.org>
2020-01-21 11:59:34 -0800

    Added the program ForLoop Running total Part 2

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,5 +1,18 @@
 """
 Programmer: Zach Anderson
 Date: 12.16.19
+Running total Part 2
 """
 
+sum = 0
+number_of_numbers = int(input("How many numbers would you like to sum? "))
+
+print("")
+
+
+for i in range(number_of_numbers):
+    next = int(input("Enter a number: "))
+    sum = sum + next
+
+print ("")
+print ("Sum: " + str(sum))
