64c98ffdf1aa02e934404f4b63f9be9735ac0771
zach <02zachary.anderson@gulllakecs.org>
2020-02-04 04:48:47 -0800

    Add Functions & Variable = printSomething

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,5 +1,25 @@
-"""
-Programmer: Zach Anderson
-Date: 12.16.19
-"""
+# Date: 2/3/20
+# Greeting Function
 
+# Declare Global Variables here......
+
+name = input("\nWhat is your name: ")
+x = 15
+
+
+# Create Functions Here......
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Very nice to meet you " + name)
+    print(x)
+
+def printsomething():
+    x = 3
+    print(x)
+
+# Call Functions Here......
+
+greeting()
+printsomething()
+print(x)
